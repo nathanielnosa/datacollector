1b0e7b7472e83527af20eb91535d2282e637e96f
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-12 03:07:10 +0100

    data field view and urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('register/',views.RegisterView.as_view())
+    path('register/',views.RegisterView.as_view()),
+    path('entry/',views.DataFieldView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -15,6 +15,23 @@ class RegisterView(APIView):
                 return Response({"message": "User registered successfully."}, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({"error":"something went wrong"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
- 
+class DataFieldView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        try:
+            entry = DataField.objects.filter(user=request.user)
+            serializer = DataFieldSerializer(entry, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def post(self,request):
+        try:
+            serializer = DataFieldSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
