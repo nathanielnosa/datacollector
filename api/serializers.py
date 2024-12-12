a68994461bdc96b14259a728be003fec88fff81c
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-12 02:03:50 +0100

    registration view and url and serializer adjustment

--- api/serializers.py
+++ api/serializers.py
@@ -7,15 +7,20 @@ class RegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username','password1','password2','email']
-    def verify(self, data):
+        fields = ['username','password1','password2','email','first_name','last_name']
+    def validate(self, data):
+        if not data['first_name']:
+            raise serializers.ValidationError({'message':'This field is required'})
         if data['password1'] != data['password2']:
             raise serializers.ValidationError({'message':'Password not match'})
+        return data
     def create(self,validate_data):
         username = validate_data['username']
         password = validate_data['password1']
         email = validate_data['email']
-        user = User.objects.create_user(username=username,password=password,email=email)
+        first_name = validate_data['first_name']
+        last_name = validate_data['last_name']
+        user = User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
         return user
 
 class DataFieldSerializer(serializers.ModelSerializer):
@@ -26,8 +31,8 @@ class DataFieldSerializer(serializers.ModelSerializer):
     def validate(self,value):
         if not value['location']:
             raise serializers.ValidationError({"message":"This field is required."})
-        if not value['amount_paid']:
+        if not value['amount_paid'] or value['amount_paid'] <= 0:
             raise serializers.ValidationError({"message":"Amount paid must be greater than zero."})
-        if not value['volume_dispensed'] or value['volume_dispensed'] <=0:
+        if not value['volume_dispensed'] or value['volume_dispensed'] <= 0:
             raise serializers.ValidationError({"message":"Volume dispensed must be between 1 and 1000."})
         return value
--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import DataField
+from .serializers import DataFieldSerializer, RegisterSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        try:
+            serializer = RegisterSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message": "User registered successfully."}, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error":"something went wrong"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+ 
